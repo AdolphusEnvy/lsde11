f271f9f47e6c40597aa79f213217dc7e4b8b86fb
AdolphusEnvy <hy8227852@163.com>
2018-10-05 10:41:29 +0200

    fix import

--- hadoopcryptoledger-master/examples/scala-spark-graphx-bitcointransaction/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinTransactionGraph.scala
+++ hadoopcryptoledger-master/examples/scala-spark-graphx-bitcointransaction/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinTransactionGraph.scala
@@ -19,7 +19,7 @@ package org.zuinnote.spark.bitcoin.example
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.hadoop.conf._
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
 import org.apache.hadoop.mapreduce._
