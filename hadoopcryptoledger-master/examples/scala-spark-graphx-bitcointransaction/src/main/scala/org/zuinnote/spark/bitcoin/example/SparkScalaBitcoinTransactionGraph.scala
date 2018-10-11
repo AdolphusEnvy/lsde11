9b0453ded8a17a73c14c047227e9257fcd1da0aa
AdolphusEnvy <hy8227852@163.com>
2018-10-11 15:33:29 +0200

    Update hadoopcryptoledger-master/examples/scala-spark-graphx-bitcointransaction/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinTransactionGraph.scala

--- hadoopcryptoledger-master/examples/scala-spark-graphx-bitcointransaction/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinTransactionGraph.scala
+++ hadoopcryptoledger-master/examples/scala-spark-graphx-bitcointransaction/src/main/scala/org/zuinnote/spark/bitcoin/example/SparkScalaBitcoinTransactionGraph.scala
@@ -93,7 +93,6 @@ object SparkScalaBitcoinTransactionGraph {
 		centralTranscations.show(10)
 		val outputSourceNames=Seq("dest_address","curr_trans_input_hash","curr_trans_input_output_idx","curr_trans_hash","curr_trans_output_idx","timestamp","value")
 		val inputSourceNames=Seq("source_address","source_trans_input_hash","source_trans_input_output_idx","source_trans_hash","source_trans_output_idx","source_timestamp","source_value")
-		//btcDF.show(10)
 		val sourceDF=btcDF.select($"dest_address".alias("source_address"), $"curr_trans_input_hash".alias("source_trans_input_hash"),$"curr_trans_input_output_idx".alias("source_trans_input_output_idx"),$"curr_trans_hash".alias("source_trans_hash"), $"curr_trans_output_idx".alias("source_trans_output_idx"),$"timestamp".alias("source_timestamp"),$"value".alias("source_value"))
 		val joined_degree1=sameTranscation.join(sourceDF,sameTranscation("curr_trans_input_hash")===sourceDF("source_trans_hash")&&centralTranscations("curr_trans_input_output_idx")===sourceDF("source_trans_output_idx"))
 		joined_degree1.show(10)
@@ -101,20 +100,20 @@ object SparkScalaBitcoinTransactionGraph {
 
 
 		val source_degree1=joined_degree1.select($"source_address".alias("dest_address"), $"source_trans_input_hash".alias("curr_trans_input_hash"),$"source_trans_input_output_idx".alias("curr_trans_input_output_idx"),$"source_trans_hash".alias("curr_trans_hash"), $"source_trans_output_idx".alias("curr_trans_output_idx"),$"source_timestamp".alias("timestamp"),$"source_value".alias("value"))
-		val same_degree1=source_degree1.join(btcDF,source_degree1("curr_trans_hash")===btcDF("curr_trans_hash"))
+		val same_degree1=btcDF.filter(source_degree1("curr_trans_hash")===btcDF("curr_trans_hash"))
 		val joined_degree2=same_degree1.join(sourceDF,same_degree1("curr_trans_input_hash")===sourceDF("source_trans_hash")&&same_degree1("curr_trans_input_output_idx")===sourceDF("source_trans_output_idx"))
 		joined_degree2.show(10)
 		joined_degree2.select($"dest_address",$"value",$"source_address",$"source_value",$"timestamp").distinct.write.format("com.databricks.spark.csv").option("header", "true").save(outputFile+"/"+back_type+"/degree2.csv")
 
 
 		val source_degree2=joined_degree2.select($"source_address".alias("dest_address"), $"source_trans_input_hash".alias("curr_trans_input_hash"),$"source_trans_input_output_idx".alias("curr_trans_input_output_idx"),$"source_trans_hash".alias("curr_trans_hash"), $"source_trans_output_idx".alias("curr_trans_output_idx"),$"source_timestamp".alias("timestamp"),$"source_value".alias("value"))
-		val same_degree2=source_degree2.join(btcDF,source_degree2("curr_trans_hash")===btcDF("curr_trans_hash"))
+		val same_degree2=btcDF.filter(source_degree2("curr_trans_hash")===btcDF("curr_trans_hash"))
 		val joined_degree3=same_degree2.join(sourceDF,same_degree2("curr_trans_input_hash")===sourceDF("source_trans_hash")&&same_degree2("curr_trans_input_output_idx")===sourceDF("source_trans_output_idx"))
 		joined_degree2.show(10)
 		joined_degree2.select($"dest_address",$"value",$"source_address",$"source_value",$"timestamp").distinct.write.format("com.databricks.spark.csv").option("header", "true").save(outputFile+"/"+back_type+"/degree3.csv")
 
 		val source_degree3=joined_degree3.select($"source_address".alias("dest_address"), $"source_trans_input_hash".alias("curr_trans_input_hash"),$"source_trans_input_output_idx".alias("curr_trans_input_output_idx"),$"source_trans_hash".alias("curr_trans_hash"), $"source_trans_output_idx".alias("curr_trans_output_idx"),$"source_timestamp".alias("timestamp"),$"source_value".alias("value"))
-		val same_degree3=source_degree3.join(btcDF,source_degree3("curr_trans_hash")===btcDF("curr_trans_hash"))
+		val same_degree3=btcDF.filter(source_degree3("curr_trans_hash")===btcDF("curr_trans_hash"))
 
 		val joined_degree4=same_degree3.join(sourceDF,same_degree3("curr_trans_input_hash")===sourceDF("source_trans_hash")&&same_degree3("curr_trans_input_output_idx")===sourceDF("source_trans_output_idx"))
 		joined_degree2.show(10)
